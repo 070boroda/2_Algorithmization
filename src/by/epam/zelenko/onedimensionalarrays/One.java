8014a5ce9caf502ff174e978f4a108f111576ec5
070boroda <zely1984@gmail.com>
2019-07-23 21:08:23 +0300

    add one task

--- src/by/epam/zelenko/onedimensionalarrays/One.java
+++ src/by/epam/zelenko/onedimensionalarrays/One.java
@@ -1,5 +1,17 @@
 package by.epam.zelenko.onedimensionalarrays;
 
 public class One {
+    public static void main(String[] args) {
+        int[] a ={2,4,7,4,9,1,6};
+        int k = 2;
+        int count = 0;
 
+        for (int i = 0; i < a.length; i++){
+            int c = a[i]%k;
+            if(c==0){
+               count +=a[i];
+            }
+        }
+        System.out.println(count);
+    }
 }
