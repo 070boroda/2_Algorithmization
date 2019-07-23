7cc1f7e1ee00f4783f206f2a6f0d6f41a9ca530d
070boroda <zely1984@gmail.com>
2019-07-23 21:16:51 +0300

    add two task

---
+++ src/by/epam/zelenko/onedimensionalarrays/Two.java
@@ -0,0 +1,21 @@
+package by.epam.zelenko.onedimensionalarrays;
+
+public class Two {
+
+    public static void main(String[] args) {
+        int[] a = {4,2,6,7,8,4,1,12};
+        int b = 5;
+        int count = 0;
+
+        for (int i = 0; i<a.length; i++){
+            if(b<a[i]){
+               a[i] = b;
+               count++;
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("Kolichestvo zamen " + count);
+    }
+}
