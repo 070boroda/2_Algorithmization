40524285b7c921b9f3ae2dc65011f14dbc55779f
070boroda <zely1984@gmail.com>
2020-01-15 20:07:47 +0300

    add 3

---
+++ src/by/epam/zelenko/onedimensionalarrays/Three.java
@@ -0,0 +1,25 @@
+package by.epam.zelenko.onedimensionalarrays;
+
+public class Three {
+
+    public static void main(String[] args) {
+        int[] a = {-2, -2, 6, -7, 8, -4, 1, 12,0,0};
+        int countZero = 0;
+        int countNegative = 0;
+        int countPositive = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0 ) {
+                countZero++;
+            } else if(a[i]>0){
+                countPositive++;
+            }else {
+                countNegative++;
+            }
+
+        }
+        System.out.println("Positive number " + countPositive);
+        System.out.println("Negative number " + countNegative);
+        System.out.println("Zero " + countZero);
+    }
+}
