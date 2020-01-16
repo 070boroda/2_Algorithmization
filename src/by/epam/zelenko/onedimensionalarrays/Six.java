97a8242842f2c0749944b30abaf56135961c87bd
070boroda <zely1984@gmail.com>
2020-01-16 18:09:36 +0300

    add 6

---
+++ src/by/epam/zelenko/onedimensionalarrays/Six.java
@@ -0,0 +1,29 @@
+package by.epam.zelenko.onedimensionalarrays;
+
+public class Six {
+
+    public static void main(String[] args) {
+
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (isPrime(i)) {
+                sum += array[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static boolean isPrime(int x) {
+        if (x < 2) return false;
+        int temp;
+        for (int i = 2; i <= x / 2; i++) {
+            temp = x % i;
+            if (temp == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
